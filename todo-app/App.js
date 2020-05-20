d4e673810840c5a8ed5ca0adaa799cb44a125d21
sgkolli535 <59209263+sgkolli535@users.noreply.github.com>
2020-05-19 20:15:12 -0700

    Update App.js

--- todo-app/App.js
+++ todo-app/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
+import InputBar from './components/InputBar';
+import TodoItem from './components/TodoItem';
+
 export default class App extends React.Component {
   constructor () {
     super();
@@ -13,7 +16,44 @@ export default class App extends React.Component {
       ]
     }
   }
+  
+  addNewTodo () {
+    let todos = this.state.todos;
 
+    todos.unshift({
+      id: todos.length + 1,
+      title: this.state.todoInput,
+      done: false
+    });
+
+    this.setState({
+      todos: todos,
+      todoInput: ''
+    });
+  }
+
+  toggleDone (item) {
+    let todos = this.state.todos;
+
+    todos = todos.map((todo) => {
+      if (todo.id == item.id) {
+        todo.done = !todo.done;
+      }
+
+      return todo;
+    })
+
+    this.setState({todos});
+  }
+
+  removeTodo (item) {
+    let todos = this.state.todos;
+
+    todos = todos.filter((todo) => todo.id !== item.id);
+
+    this.setState({todos});
+  }
+  
   render() {
     const statusbar = (Platform.OS == 'ios') ? <View style={styles.statusbar}></View> : <View></View>;
     
@@ -21,7 +61,23 @@ export default class App extends React.Component {
       <View style={styles.container}>
         {statusbar}
 
-        <Header title= />
+        <Header title="Todoapp" />
+          
+        <InputBar 
+         addNewTodo{() => this.addNewTodo()}
+         textChange={todoInput => this.setState({ todoInput })}
+         todoInput={this.state.todoInput} />
+    
+        <FlatList
+          data={this.state.todos}
+          extraData={this.state}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item, index}) => {
+            return (
+              <TodoItem todoItem={item} toggleDone={() => this.toggleDone(item)} removeTodo={() => this.removeTodo(item)} />
+            )
+          }}
+        />
       </View>
     );
   }
